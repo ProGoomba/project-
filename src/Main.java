44254df65350ae286c71b7df10aa1f3fe75cd191
ProGoomba <donaldrayglover@yahoo.com>
2026-09-16 19:22:48 -0400

    Check-in

--- src/Main.java
+++ src/Main.java
@@ -4,15 +4,17 @@ import java.util.Scanner;
 
 class Main
 {
-    void main(String[] Args)
+    public static void main(String[] Args)
     {
 
         //Pass The Story Project
         //This program takes string input and assigns it to null spaces within the WordList array!
 
         Scanner sc = new Scanner(System.in);
-        String[] WordList = new String[10];
-        int i = 0;
+        String[] WordList = new String[6];
+        String firstEntry = "Once upon a time, there was a Hare with 8 lives";
+        WordList[0] = firstEntry;
+        int i = 1;
 
         //Assigns String inputs to free spaces within the Array.
         while (i < WordList.length) {
@@ -23,21 +25,24 @@ class Main
                 String wordInput = sc.nextLine();
 
                 //Special characters or numbers? ACCESS DENIED! >:C
-                if(!wordInput.matches("[a-zA-Z]+"))
+                if(!wordInput.endsWith("and then."))
                 {
-                    throw new IllegalArgumentException("Please only input strings or alphabetical numbers." + "\nNo Special characters");
+                    throw new IllegalArgumentException("End of sentence must end with \"and then.\"");
                 }
 
                 //WordCheck Passed Allocating String input to Array Space.
                 WordList[i] = wordInput;
                 i++;
 
-                //Prints out each and every word assigned from Array index 0 to 9
+                //Prints every Non-null story entry into the Array
                 if (i == WordList.length)
                 {
                     for (int a = 0; a < WordList.length; )
                     {
-                        System.out.println(WordList[a] + " ");
+                        if(WordList[a] != null) {
+                            System.out.println(WordList[a] + " ");
+
+                        }
                         a++;
                     }
                 }
