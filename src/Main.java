25cd4b047d737ececb277f5d252235e96fdba8b4
ProGoomba <donaldrayglover@yahoo.com>
2026-09-16 18:54:00 -0400

    Check-in

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,55 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    IO.println(String.format("Hello and welcome!"));
 
-    for (int i = 1; i <= 5; i++) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        IO.println("i = " + i);
+import java.util.Scanner;
+
+
+class Main
+{
+    void main(String[] Args)
+    {
+
+        //Pass The Story Project
+        //This program takes string input and assigns it to null spaces within the WordList array!
+
+        Scanner sc = new Scanner(System.in);
+        String[] WordList = new String[10];
+        int i = 0;
+
+        //Assigns String inputs to free spaces within the Array.
+        while (i < WordList.length) {
+
+            //Non-String input handling via: Try, Catch.
+            try
+            {
+                String wordInput = sc.nextLine();
+
+                //Special characters or numbers? ACCESS DENIED! >:C
+                if(!wordInput.matches("[a-zA-Z]+"))
+                {
+                    throw new IllegalArgumentException("Please only input strings or alphabetical numbers." + "\nNo Special characters");
+                }
+
+                //WordCheck Passed Allocating String input to Array Space.
+                WordList[i] = wordInput;
+                i++;
+
+                //Prints out each and every word assigned from Array index 0 to 9
+                if (i == WordList.length)
+                {
+                    for (int a = 0; a < WordList.length; )
+                    {
+                        System.out.println(WordList[a] + " ");
+                        a++;
+                    }
+                }
+
+            }
+            catch(IllegalArgumentException e)
+            {
+                System.out.println("Error: " + e.getMessage());
+
+            }
+
+
+        }
     }
 }
